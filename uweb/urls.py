25e947d9b3980d8e6ce3488e0894b2f14fce16e9
Ahmad Ghulam Zakiy <ghulam.zakiy@gmail.com>
2026-05-13 22:58:50 +0800

    tidying debug options

--- uweb/urls.py
+++ uweb/urls.py
@@ -19,10 +19,12 @@ urlpatterns = [
 if settings.DEBUG:
     from django.conf.urls.static import static
     from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+    from debug_toolbar.toolbar import debug_toolbar_urls
 
     # Serve static and media files from development server
     urlpatterns += staticfiles_urlpatterns()
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += debug_toolbar_urls()
 
 urlpatterns = urlpatterns + [
     # For anything not caught by a more specific rule above, hand over to
@@ -35,7 +37,3 @@ urlpatterns = urlpatterns + [
 ]
 
 
-from debug_toolbar.toolbar import debug_toolbar_urls
-
-if settings.DEBUG:
-    urlpatterns += debug_toolbar_urls()
