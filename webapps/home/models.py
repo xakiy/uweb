845b31c25b64633ff7cc5527eb469e3655afdfb3
Ahmad Ghulam Zakiy <ghulam.zakiy@gmail.com>
2026-05-17 23:28:20 +0800

    Styling p in copyright section smaller Fix requirements.txt to include some fontawesome packages

--- webapps/home/models.py
+++ webapps/home/models.py
@@ -12,12 +12,16 @@ from wagtail.admin.panels import (
     PublishingPanel,
 )
 from wagtail.models import (
-    Page,
-    Task,
+    Collection,
     DraftStateMixin,
+    LockableMixin,
+    Page,
     PreviewableMixin,
     RevisionMixin,
+    Task,
+    TaskState,
     TranslatableMixin,
+    WorkflowMixin,
 )
 from wagtail.fields import RichTextField
 from wagtail.contrib.settings.models import (
@@ -25,6 +29,7 @@ from wagtail.contrib.settings.models import (
     BaseSiteSetting,
     register_setting,
 )
+from wagtail.search import index
 
 
 class HomePage(Page):
@@ -65,6 +70,135 @@ class SocialMedia(ClusterableModel, BaseGenericSetting):
     ]
 
 
+class Person(
+    WorkflowMixin,
+    DraftStateMixin,
+    LockableMixin,
+    RevisionMixin,
+    PreviewableMixin,
+    index.Indexed,
+    ClusterableModel,
+):
+    """
+    A Django model to store Person objects.
+    It is registered using `register_snippet` as a function in wagtail_hooks.py
+    to allow it to have a menu item within a custom menu item group.
+
+    `Person` uses the `ClusterableModel`, which allows the relationship with
+    another model to be stored locally to the 'parent' model (e.g. a PageModel)
+    until the parent is explicitly saved. This allows the editor to use the
+    'Preview' button, to preview the content, without saving the relationships
+    to the database.
+    https://github.com/wagtail/django-modelcluster
+    """
+
+    first_name = models.CharField("First name", max_length=254)
+    last_name = models.CharField("Last name", max_length=254)
+    job_title = models.CharField("Job title", max_length=254)
+
+    image = models.ForeignKey(
+        "wagtailimages.Image",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+",
+    )
+
+    workflow_states = GenericRelation(
+        "wagtailcore.WorkflowState",
+        content_type_field="base_content_type",
+        object_id_field="object_id",
+        related_query_name="person",
+        for_concrete_model=False,
+    )
+
+    revisions = GenericRelation(
+        "wagtailcore.Revision",
+        content_type_field="base_content_type",
+        object_id_field="object_id",
+        related_query_name="person",
+        for_concrete_model=False,
+    )
+
+    panels = [
+        MultiFieldPanel(
+            [
+                FieldRowPanel(
+                    [
+                        FieldPanel("first_name"),
+                        FieldPanel("last_name"),
+                    ]
+                )
+            ],
+            "Name",
+        ),
+        FieldPanel("job_title"),
+        FieldPanel("image"),
+        PublishingPanel(),
+    ]
+
+    search_fields = [
+        index.SearchField("first_name"),
+        index.SearchField("last_name"),
+        index.FilterField("job_title"),
+        index.AutocompleteField("first_name"),
+        index.AutocompleteField("last_name"),
+    ]
+
+    @property
+    def thumb_image(self):
+        # Returns an empty string if there is no profile pic or the rendition
+        # file can't be found.
+        try:
+            return self.image.get_rendition("fill-50x50").img_tag()
+        except:  # noqa: E722 FIXME: remove bare 'except:'
+            return ""
+
+    @property
+    def preview_modes(self):
+        return PreviewableMixin.DEFAULT_PREVIEW_MODES + [("blog_post", _("Blog post"))]
+
+    def __str__(self):
+        return "{} {}".format(self.first_name, self.last_name)
+
+    def get_preview_template(self, request, mode_name):
+        from bakerydemo.blog.models import BlogPage
+
+        if mode_name == "blog_post":
+            return BlogPage.template
+        return "base/preview/person.html"
+
+    def get_preview_context(self, request, mode_name):
+        from bakerydemo.blog.models import BlogPage
+
+        context = super().get_preview_context(request, mode_name)
+        if mode_name == self.default_preview_mode:
+            return context
+
+        page = BlogPage.objects.filter(blog_person_relationship__person=self).first()
+        if page:
+            # Use the page authored by this person if available,
+            # and replace the instance from the database with the edited instance
+            page.authors = [
+                self if author.pk == self.pk else author for author in page.authors()
+            ]
+            # The authors() method only shows live authors, so make sure the instance
+            # is included even if it's not live as this is just a preview
+            if not self.live:
+                page.authors.append(self)
+        else:
+            # Otherwise, get the first page and simulate the person as the author
+            page = BlogPage.objects.first()
+            page.authors = [self]
+
+        context["page"] = page
+        return context
+
+    class Meta:
+        verbose_name = "Person"
+        verbose_name_plural = "People"
+
+
 class CopyrightText(
     DraftStateMixin,
     RevisionMixin,
@@ -95,7 +229,7 @@ class CopyrightText(
     ]
 
     def __str__(self):
-        return "Copyright text"
+        return "Copyright Text"
 
     def get_preview_template(self, request, mode_name):
         return "base.html"
