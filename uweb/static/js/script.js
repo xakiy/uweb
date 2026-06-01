61b913c5fe3d08fa59424650907922f2f0ca7aad
Ahmad Ghulam Zakiy <ghulam.zakiy@gmail.com>
2026-06-01 18:04:13 +0800

    stupid sticky nav finalization

--- uweb/static/js/script.js
+++ uweb/static/js/script.js
@@ -1,22 +1,17 @@
-/**
- * Header fixed top on scroll
- */
-let selectHeader = document.querySelector('#main-header');
-if (selectHeader) {
-  let breadcrumbDiv = document.querySelector('#main-breadcrumb')
-  const scrollHeight = breadcrumbDiv.offsetTop
-  const headerFixed = () => {
-    if (window.scrollY > scrollHeight) {
-      selectHeader.classList.add('fixed-top', 'shadow-sm');
-      breadcrumbDiv.style.paddingTop=scrollHeight+"px";
+/** Sticky Navbar **/
+let mainHeader = document.querySelector('#main-header')
+if (mainHeader) {
+  const stickyNav = () => {
+    if (window.scrollY > 50) {
+        mainHeader.classList.add('shadow-sm');
     } else {
-      selectHeader.classList.remove('fixed-top', 'shadow-sm')
-      breadcrumbDiv.style.paddingTop=0;
+        mainHeader.classList.remove('shadow-sm');
     }
   }
-  window.addEventListener('load', headerFixed)
-  document.addEventListener('scroll', headerFixed)
+  window.addEventListener('load', stickyNav)
+  document.addEventListener('scroll', stickyNav)
 }
+
 
 /** Back to top button **/
 let backtotop = document.querySelector('.back-to-top')
@@ -31,23 +26,3 @@ if (backtotop) {
   window.addEventListener('load', toggleBacktotop)
   document.addEventListener('scroll', toggleBacktotop)
 }
-
-// /** Sticky Navbar **/
-// let mainHeader = document.querySelector('#main-header')
-// if (mainHeader) {
-//   const stickyNav = () => {
-//     if (window.scrollY > 100) {
-//         mainHeader.style.position='fixed';
-//         mainHeader.style.top='0px';
-//         mainHeader.classList.add('shadow-sm');
-//         // mainHeader.classList.add('fixed-top');
-//     } else {
-//         mainHeader.style.position='initial';
-//         mainHeader.style.top='-150px';
-//         mainHeader.classList.remove('shadow-sm');
-//         // mainHeader.classList.remove('fixed-top');
-//     }
-//   }
-//   window.addEventListener('load', stickyNav)
-//   document.addEventListener('scroll', stickyNav)
-// }
